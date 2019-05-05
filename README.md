1f365e91649949bc9009c92015bfcd1bc5d05824
xedom <xedom08@gmail.com>
2019-05-05 10:18:28 +0200

    steam login

---
+++ README.md
@@ -0,0 +1,9 @@
+### config.js
+```js
+module.exports = {
+    server: {
+        port: 8801,
+        steam_token: '8E5D5A4C69D001BA20C1B71834DD0C56'
+    }
+}
+```
--- api/user.js
+++ api/user.js
@@ -1,6 +1,54 @@
 const express = require('express');
 const router = express.Router();
 
+const cfg = require('../config');
+
+const passport = require('passport');
+const SteamStrategy = require('passport-steam').Strategy;
+
+router.use(passport.initialize())
+
+
+
+passport.use(new SteamStrategy({
+    returnURL: 'http://localhost:8801/user/steam/return',
+    realm: 'http://localhost:8801/user/',
+    apiKey: cfg.server.steam_token
+  },
+  function(identifier, profile, done) {
+    const user = {
+        openid: identifier,
+        steam_id: profile._json.steamid,
+        name: profile._json.personaname,
+        dateTest: profile._json.lastlogoff,
+        profile_url: profile._json.profileurl,
+        avatar: profile._json.avatar,
+    }
+    // console.log(user);
+    return done(null,user);
+    // User.findByOpenID({ openId: identifier }, function (err, user) {
+    //   return done(err, user);
+    // });
+  }
+));
+
+router.get('/steam',
+  passport.authenticate('steam'),
+  function(req, res) {
+      console.log('get /steam');
+    // The request will be redirected to Steam for authentication, so
+    // this function will not be called.
+  });
+
+router.get('/steam/return',
+  passport.authenticate('steam', { failureRedirect: '/user/steam' }),
+  function(req, res) {
+    console.log(req.user)
+    console.log('get /steam/return');
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
+
 router.get('/', (req, res) => {
     res.json({ status: 'user ok' });
 });
