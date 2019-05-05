cc9c6a0872ba0e1eddee05a5bce5484e33104cc1
xedom <xedom08@gmail.com>
2019-05-05 02:34:38 +0200

    server api

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const cfg = require('../config');
 
+app.use('/server',require('../api/server'));
+
 app.listen(cfg.server.port, () => {
     console.log(`[SERVER] > Server started on ${cfg.server.port}`);
 });
