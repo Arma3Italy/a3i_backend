77492033715045e3d236ddf3fc61736568a21731
Kaj Oskar Rusilowski <xedom08@gmail.com>
2019-05-10 10:09:43 +0200

    Update auth.js

--- api/auth.js
+++ api/auth.js
@@ -1,43 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 
 const cfg = require('../config');
+const passportConfig = require('../config/passport.js');
 
-const passport = require('passport');
-const SteamStrategy = require('passport-steam').Strategy;
-
-router.use(passport.initialize())
-
-passport.use(new SteamStrategy({
-    returnURL: 'http://localhost:8801/user/steam/return',
-    realm: 'http://localhost:8801/user/',
-    apiKey: cfg.server.steam_token
-  },
-  function(identifier, profile, done) {
-    const user = {
-        openid: identifier,
-        steam_id: profile._json.steamid,
-        name: profile._json.personaname,
-        dateTest: profile._json.lastlogoff,
-        profile_url: profile._json.profileurl,
-        avatar: profile._json.avatar,
-    }
-    // save user to db
-    
-    return done(null,user);
-  }
-));
-
-passport.serializeUser(function(user, done) {
-  done(null, user.openid);
-  // console.log(user);
-});
-
-passport.deserializeUser(function(id, done) {
-  // User.findById(id, function(err, user) {
-  //     done(err, user);
-  // });
-});
+router.use(passport.initialize());
+passportConfig();
 
 router.get('/steam',
   passport.authenticate('steam', {
