6eb4274c2b5b9b8030056e97cc08ac8a3e954a92
Kaj Oskar Rusilowski <Kaj Oskar Rusilowski>
2019-05-11 00:07:29 +0200

    bho

--- api/auth.js
+++ api/auth.js
@@ -7,44 +7,27 @@ console.log('[EXPRESS] > auth route');
 const passportConfig = require('../config/passport.js');
 
 
+
 router.use(passport.initialize());
 passportConfig();
 
-router.get('/steam',(req, res, next) => {
-    console.log('[AUTH] > (GET /auth/steam)');
-    next();
-  },
-  passport.authenticate('steam', {
-    successRedirect: '/success',
-    failureRedirect: '/fail',
-    session: false
-  },() => {
-    console.log('[PASSPORT] > (GET /auth/steam) authenticate callback');
-  }),
-  function(req, res) {
-    console.log('[PASSPORT] > (GET /auth/steam) next function');
-  });
+router.get('/steam',
+  passport.authenticate('steam')
+);
 
-router.get('/steam/return',(req, res, next) => {
-    console.log('[AUTH] > (GET /auth/steam/return)');
-    next();
-  },
-  passport.authenticate('steam', {
-    successRedirect: '/success',
-    failureRedirect: '/fail',
-    session: false
-  },() => {
-    console.log('[PASSPORT] > (GET /auth/steam/return) authenticate callback');
-  }),
+router.get('/steam/return',
   function(req, res) {
+    console.log(req.session)
+    console.log("-------------------")
+    console.log(req.user)
+    console.log("-------------------")
+    console.log(req.passport)
+    console.log("-------------------")
     console.log('[PASSPORT] > (GET /auth/steam/return) next function');
-    res.redirect('/');
+    res.json({ status: "OK" });
   });
 
-router.get('/',(req, res, next) => {
-    console.log('[AUTH] > (GET /auth)');
-    next();
-  }, (req, res) => {
+router.get('/', (req, res) => {
     console.log('[AUTH] > (GET /auth) next function');
     res.json({ status: 'user ok' });
 });
