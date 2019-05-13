c642afe8ecb4d7542ea735905b1fcde7b4ea1dc1
xedom <xedom08@gmail.com>
2019-05-14 01:08:36 +0200

    ,

--- src/index.js
+++ src/index.js
@@ -2,15 +2,17 @@ const express = require('express');
 const app = express();
 const session = require('express-session');
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser')
 const { server: { port, sessionKey, mongoUri } } = require('../config.js');
 
+app.use(cookieParser());
 app.use(session({
     secret: sessionKey,
     saveUninitialized: false,
     resave: false,
     cookie: {
         httpOnly: true,
-        secure: false
+        secure: false,
     }
 }));
 
