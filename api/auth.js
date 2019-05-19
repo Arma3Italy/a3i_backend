04d509ed309af95b0d81ec9b8f1c8aeb1b915618
xedom <xedom08@gmail.com>
2019-05-20 00:05:56 +0200

    fix db error

--- api/auth.js
+++ api/auth.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const steamLogin = require('steam-login');
 const { steam: steamcfg } = require('../config.js');
 const UserModel = require('../models/User.js');
+const { devLog } = require('../util');
 
 router.use(steamLogin.middleware(steamcfg));
 
@@ -19,41 +20,43 @@ router.get('/steam', steamLogin.authenticate());
 router.get('/steam/return', steamLogin.verify(), (req, res, next) => {
   const steamid = req.user._json.steamid;
 
-  UserModel.findOne({ steamid }).exec((err, usr) => {
-    if (!err) {
-      req.user = usr;
-      return next();
-    };
-  });
+  return UserModel.findOne({ steamid }).then(usr => {
+    devLog('MONGO','search user')
+    let user;    
 
-  console.log('[SERVER] > New User');
+    if(usr){
+      devLog('MONGO','user found')
 
-  const user = new UserModel({
-    steamid: req.user._json.steamid,
-    name: req.user._json.personaname,
-    url: req.user._json.profileurl,
-    avatar: req.user.avatar.medium
-  });
+      user = usr;
+    } else {
+      devLog('MONGO','user not found')
 
-  req.user = user;
-  next();
- 
-}, (req, res) => {
-  console.log('[SERVER] > User loaded');
+      user = new UserModel({
+        steamid: req.user._json.steamid,
+        name: req.user._json.personaname,
+        url: req.user._json.profileurl,
+        avatar: req.user.avatar.medium
+      });
+    }
 
-  const user = req.user;
-  const token = user.newSession(req.sessionID,'0.0.0.0');
+    const token = user.newSession(req.sessionID,'0.0.0.0');
+    user.save();
 
-  user.save();
-  // res.cookie('authToken', token, { domain: '.example.com', path: '/admin', secure: true, expires: new Date(Date.now() + 900000), });
-  res.cookie('authToken', token,);
-  req.session.a3i_user = user;
+    // res.clearCookie('authToken');
+    // res.cookie('authToken', token, { domain: '.example.com', path: '/admin', secure: true, expires: new Date(Date.now() + 900000), });
+    res.cookie('authToken', token,);
+    // req.session.a3i_user = user;
+  
+    return res.json({
+      sessionID: req.sessionID,
+      token,
+      session: req.session,
+      // user
+    });
 
-  res.json({
-    sessionID: req.sessionID,
-    token,
-    session: req.session,
-    // user
+  }).catch(err => {
+    devLog('MONGO','error found');
+    devLog('MONGO',`{${err.name}} ${err.message}`);
   });
 });
 
