5153e2e42c8d1451ee0400427ca304ac00411320
Kaj Oskar Rusilowski <xedom08@gmail.com>
2019-06-26 19:18:29 +0200

    console replace by devlog

--- api/auth.js
+++ api/auth.js
@@ -109,11 +109,11 @@ router.get('/testSession', (req, res) => {
 
 
 
-router.get('/testSession', (req, res) => {
+router.get('/testIP', (req, res) => {
   let ip = req.headers['cf-connecting-ip'] || req.headers['x-forwarded-for'] || req.connection.remoteAddress;
   ip = ip.split(':')[3];
   
-  console.log(ip)
+  devLog('[/testIP]',ip)
   
   res.json({ ip })
 });
